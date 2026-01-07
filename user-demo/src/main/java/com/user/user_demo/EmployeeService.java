d1883fad017d31be7de2361b7df105c2c8204b74
Vikram2511 <vikramreddy2511@gmail.com>
2026-01-07 23:13:47 +0530

    updated empservice class

--- user-demo/src/main/java/com/user/user_demo/EmployeeService.java
+++ user-demo/src/main/java/com/user/user_demo/EmployeeService.java
@@ -1,7 +1,10 @@
 package com.user.user_demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeService {
+    @Autowired
+    private EmployeeRepository employeeRepository;
 }
