499555a78d759c58aece392ec872b1ac41075530
Vikram2511 <vikramreddy2511@gmail.com>
2026-01-08 22:58:03 +0530

    Revert "updated empservice class"
    This reverts commit d1883fad017d31be7de2361b7df105c2c8204b74.

--- user-demo/src/main/java/com/user/user_demo/EmployeeService.java
+++ user-demo/src/main/java/com/user/user_demo/EmployeeService.java
@@ -1,10 +1,7 @@
 package com.user.user_demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeService {
-    @Autowired
-    private EmployeeRepository employeeRepository;
 }
